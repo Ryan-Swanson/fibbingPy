11c3c769955c3ba483a3bfe545b2b840c62a0d34
Ryan Swanson <67406108+Ryan-Swanson@users.noreply.github.com>
2021-07-23 00:58:25 +0000

    Code format, and order

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # Exponential fib abusing recursion at high n 
-# def fib(n):
-#   if n == 0:
-#     return 0
-#   if n == 1:
-#     return 1
-#   return fib(n-1) + fib(n-2)
+def fib(n):
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  return fib(n-1) + fib(n-2)
 
-# fibbing = int(input("Enter the fib index: "))
-
-# print(fib(fibbing))
 
 #Polynomial fib with array generation
 def fib2(n):
@@ -20,8 +17,11 @@ def fib2(n):
   f = [0,1]
   for i in range(2, n+1):
     f.append(f[i-1] + f[i-2])
-
   return f[n]
+
 
 fibbing = int(input("Enter the fib index: "))
 print(fib2(fibbing))
+
+fibbing = int(input("Enter the fib index: "))
+print(fib(fibbing))
