9ac849ba77b017039db86a40ec59473fe7611983
Ryan Swanson <67406108+Ryan-Swanson@users.noreply.github.com>
2021-08-03 16:56:50 +0000

    Added fib3 using memoization

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Exponential fib abusing recursion at high n 
 def fib(n):
   if n == 0:
@@ -19,9 +21,24 @@ def fib2(n):
     f.append(f[i-1] + f[i-2])
   return f[n]
 
+def fib3(n, memo):
+  if n in memo:
+    return memo[n]
+  if n == 1 or n == 2:
+    result = 1
+  else:
+    result = fib3(n-1, memo) + fib3(n-2, memo)
+  memo[n] = result
+  return result
+
+# fibbing = int(input("Enter the fib index: "))
+# print(fib2(fibbing))
+
+# fibbing = int(input("Enter the fib index: "))
+# print(fib(fibbing))
+memo = {}
 
 fibbing = int(input("Enter the fib index: "))
-print(fib2(fibbing))
 
-fibbing = int(input("Enter the fib index: "))
-print(fib(fibbing))
+for i in range(1,200):
+  print(f'fib({i}): ',fib3(i, memo))
